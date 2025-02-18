f671e8a8e068c174643300ff701125610a6d589a
mariosearchteam <67582052+mariosearchteam@users.noreply.github.com>
2025-02-18 15:46:58 +0100

    finished code, to test

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let bill = document.getElementById("bill");
-let tip = 1;
+let tip = 100;
 let customTip = document.getElementById("customTip");
 let people = document.getElementById("people");
 let tipAmount = document.getElementById("tipAmount");
 let totalAmount = document.getElementById("totalAmount");
 let buttons = document.querySelectorAll(".tipButton");
+let error = document.getElementById("error");
 
 bill.addEventListener("input", function () {
   calculateTotal();
@@ -21,22 +22,38 @@ buttons.forEach((button) => {
   });
 });
 
-customTip.addEventListener("input", function () {
-  setTip();
-});
+function setTip(customTipValue) {
+  tip = parseInt(customTipValue);
 
-function setTip(tip) {
-  tip = customTip.value;
-  console.log(tip);
-  calculateTotal();
+  if (customTipValue) {
+    let labels = document.querySelectorAll('label[for^="tip"]');
+    labels.forEach((label) => {
+      label.classList.remove("bg-strong-cyan", "text-cyan-900");
+    });
+    let radios = document.querySelectorAll('input[name="tip"]');
+    radios.forEach((radio) => {
+      radio.checked = false;
+    });
+    calculateTotal();
+  } else {
+    tip = 0;
+    calculateTotal();
+  }
 }
 
 function calculateTotal() {
-  if (people.value > 0) {
+  if (people.value > 0 && bill.value > 0) {
     totalAmount.innerText =
       "$ " + ((bill.value * (1 + tip / 100)) / people.value).toFixed(2);
     tipAmount.innerText =
       "$ " + ((bill.value * tip) / 100 / people.value).toFixed(2);
+    people.classList.remove("border-red-500");
+    people.classList.add("border-cyan-500");
+    error.innerText = "";
+  } else if (people.value <= 0) {
+    people.classList.add("border-red-500");
+    people.classList.remove("border-cyan-500");
+    error.innerText = "Can't be zero";
   } else {
     totalAmount.innerText = "$0.00";
     tipAmount.innerText = "$0.00";
@@ -50,4 +67,12 @@ function reset() {
   tipAmount.innerText = "$0.00";
   totalAmount.innerText = "$0.00";
   tip = 1;
+  let labels = document.querySelectorAll('label[for^="tip"]');
+  labels.forEach((label) => {
+    label.classList.remove("bg-strong-cyan", "text-cyan-900");
+  });
+  let radios = document.querySelectorAll('input[name="tip"]');
+  radios.forEach((radio) => {
+    radio.checked = false;
+  });
 }
